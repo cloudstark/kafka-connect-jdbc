708997762a57c5ce2d935d70aaecb26856e37e6c
Lars Martin <lars.martin@smb-tec.com>
2020-01-27 23:03:10 +0100

    rework docker compose to use Strimzi images and seed some initial data
    Signed-off-by: Lars Martin <lars.martin@smb-tec.com>

---
+++ init/seed.sql
@@ -0,0 +1,4 @@
+create table users(id bigint, lastname varchar(100), firstname varchar(100));
+
+insert into users(id, lastname, firstname) values(1, 'Doe', 'Jane');
+insert into users(id, lastname, firstname) values(2, 'Doe', 'John');
