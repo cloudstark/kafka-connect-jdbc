a75676063cd41b47d253204f7e0e1209f360e009
Lars Martin <lars.martin@smb-tec.com>
2020-01-28 23:02:09 +0100

    split seed scripts into schema and insert
    Signed-off-by: Lars Martin <lars.martin@smb-tec.com>

--- init/insert.sql
+++ init/insert.sql
@@ -1,4 +1,2 @@
-create table users(id bigint, lastname varchar(100), firstname varchar(100));
-
 insert into users(id, lastname, firstname) values(1, 'Doe', 'Jane');
 insert into users(id, lastname, firstname) values(2, 'Doe', 'John');
---
+++ init/schema.sql
@@ -0,0 +1,1 @@
+create table users(id bigint, lastname varchar(100), firstname varchar(100));
